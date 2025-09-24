4d9697d83972b0b9dbd9ded28a0a3b040de61476
Isaac <imana@DESKTOP-NRD0KK3.localdomain>
2025-09-24 01:33:13 -0400

    added operations

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,13 +1,23 @@
-def addition(a: float, b: float) -> float:
-    return a+ b
+class Operations:
 
-def subtraction(a: float, b: float) -> float:
-    return a- b
+    @staticmethod
+    def addition(a: float, b: float) -> float:
 
-def multiplication(a: float, b: float) -> float:
-    return a* b
+        return a + b 
 
-def division(a: float, b: float) -> float:
-    if b == 0:
-        raise ValueError("Division by zero is not allowed.")
-    return a / b
+    @staticmethod
+    def subtraction(a: float, b: float) -> float:
+
+        return a - b  
+
+    @staticmethod
+    def multiplication(a: float, b: float) -> float:
+
+        return a * b  
+
+    @staticmethod
+    def division(a: float, b: float) -> float:
+
+        if b == 0:
+            raise ValueError("Division by zero is not allowed.")
+        return a / b  
